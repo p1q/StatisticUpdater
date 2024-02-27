adedfdec9294220ccffe39a1645ef991d17d64ca
p1q <admin2k7@gmail.com>
2024-02-27 21:18:20 +0200

    Init commit

---
+++ src/main/java/com/updater/stat/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.updater.stat;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+}
